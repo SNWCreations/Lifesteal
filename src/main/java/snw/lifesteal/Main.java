498e3a11d2791aaf9ba7fe4c5844127a469663c8
ZX夏夜之风 <snwcreations@qq.com>
2022-12-30 02:30:59 +0000

    Implement recipe of heart

--- src/main/java/snw/lifesteal/Main.java
+++ src/main/java/snw/lifesteal/Main.java
@@ -1,5 +1,8 @@
 package snw.lifesteal;
 
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
@@ -11,10 +14,27 @@ public final class Main extends JavaPlugin {
         getCommand("lifesteal").setExecutor(new LifeStealCommand(this));
         getCommand("drawheart").setExecutor(new DrawHeartCommand());
         getServer().getPluginManager().registerEvents(new EventListener(this), this);
+        initRecipe();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getServer().resetRecipes();
+    }
+
+    private void initRecipe() {
+        // idi
+        // dad
+        // idi
+        // i - Netherite Ingot
+        // d - Diamond Block
+        // a - Totem Of Undying
+        ShapedRecipe recipe = new ShapedRecipe(new NamespacedKey(this, "heart"), Constants.HEART_ITEM)
+                                    .shape("idi", "dad", "idi")
+                                    .setIngredient('i', Material.NETHERITE_INGOT)
+                                    .setIngredient('d', Material.DIAMOND_BLOCK)
+                                    .setIngredient('a', Material.TOTEM_OF_UNDYING);
+        getServer().addRecipe(recipe);
     }
 }
