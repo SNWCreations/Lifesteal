e1063ae712b3a9b1cdeaee5470190e47e479a7c6
ZX夏夜之风 <snwcreations@qq.com>
2022-12-30 00:21:13 +0000

    Death event listener first implementation

---
+++ src/main/java/snw/lifesteal/EventListener.java
@@ -0,0 +1,37 @@
+package snw.lifesteal;
+
+import org.bukkit.BanList.Type;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class EventListener implements Listener {
+    private final Main main;
+
+    public EventListener(Main main) {
+        this.main = main;
+    }
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent e) {
+        // TODO check flag to continue
+        e.getEntity().spigot().respawn();
+        main.getServer().getScheduler().runTaskLater(main, () -> {
+            Player killer = e.getEntity().getKiller();
+            if (killer != null) { // or a mob killed this player?
+                AttributeInstance k = killer.getAttribute(Attribute.GENERIC_MAX_HEALTH); // killer's max health attribute instance
+                k.setBaseValue(k.getBaseValue() + 2.0); // 2 -> 1 heart
+                AttributeInstance d = e.getEntity().getAttribute(Attribute.GENERIC_MAX_HEALTH);
+                if (d.getBaseValue() - 2.0 <= 0) {
+                    d.setBaseValue(20.0); // reset
+                    main.getServer().getBanList(Type.NAME).addBan(e.getEntity().getName(), "你失败了！游戏结束了！", null, null);
+                } else {
+                    d.setBaseValue(d.getBaseValue() - 2.0); // remove a heart
+                }
+            }
+        }, 1L);
+    }
+}
